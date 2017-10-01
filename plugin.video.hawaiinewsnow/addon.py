7294b788a8058f1cbfd8493234fe232d77c400d1
NitroCSIA <nitrocsia@gmail.com>
2017-09-30 14:51:04 -1000

    Fixed HNN

--- plugin.video.hawaiinewsnow/addon.py
+++ plugin.video.hawaiinewsnow/addon.py
@@ -1,11 +1,5 @@
-import urllib, urllib2, os, re, xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs, sys, hashlib, time, random, json, datetime
-import xml.etree.ElementTree as ET
-from base64 import b64decode
+import urllib, urllib2, os, re, xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs, sys, time
 from cookielib import CookieJar
-try:
-    import StorageServer
-except:
-    import storageserverdummy as StorageServer 
 try:
     import json
 except:
@@ -14,7 +8,8 @@ except:
 
 HANDLE = int(sys.argv[1])
 PATH = sys.argv[0]
-QUALITY_TYPES = {'0':'low','1':'medium','2':'high'}
+QUALITY_TYPES = {'0':'360','1':'720','2':'1080'}
+CATEGORY_IDS = {'91610','96072','4773','188022'}
 
 def get_params():
         param=[]
@@ -35,28 +30,21 @@ def get_params():
 
 class Plugin():
     def __init__(self):
-        self.cache = StorageServer.StorageServer("hawaiinewsnow", 24)
         self.__settings__ = xbmcaddon.Addon(id='plugin.video.hawaiinewsnow')
         self.__language__ = self.__settings__.getLocalizedString
         self.home = self.__settings__.getAddonInfo('path')
-        self.regfile = xbmc.translatePath(os.path.join(self.home,'resources','reg'))
         self.icon = xbmc.translatePath( os.path.join( self.home, 'icon.png' ) )
         self.fanart = xbmc.translatePath( os.path.join( self.home, 'fanart.jpg' ) )
         self.dlpath = self.__settings__.getSetting('dlpath')
-        #self.quality = QUALITY_TYPES[self.__settings__.getSetting('quality')]
-        self.apiid = None
-        self.token = None
-        self.baseurl = "http://anappnews.vrvm.com/capi/hawaiinewsnow"
+        self.quality = QUALITY_TYPES[self.__settings__.getSetting('quality')]
+        self.apikey = "raycom-franklyapps-khnl-9f062c65"
+        self.affiliate_id = '55'
+        self.baseurl = "http://developer.worldnow.com"
 
     def resolve_url(self,url):
         print "Resolving URL: %s" % url
         item = xbmcgui.ListItem(path=url)
         xbmcplugin.setResolvedUrl(HANDLE, True, item)
-
-    def get_youtube_link(self,url):
-        match=re.compile('https?://www.youtube.com/.+?v=(.+)').findall(url)
-        link = 'plugin://plugin.video.youtube/?action=play_video&videoid='+ match[0]
-        return link
 
     def add_link(self, thumb, info, urlparams, fanart=None):
         if not fanart: fanart = self.fanart
@@ -102,95 +90,39 @@ class Plugin():
                 xbmc.executebuiltin('XBMC.Notification("Download","Error downloading file")')
 
     def do_http(self,full_url,type="GET",data=None):
-        nonce = str(int(time.time() * 1000))
-        url = full_url.split('?')[0]
-        if '?' not in full_url:
-            full_url += '?'
-        digest_str = "%s:%s:%s:%s:%s" % (self.apiid,type,url,self.token,nonce)
-        digest = hashlib.sha1(digest_str).hexdigest()
-        requrl="%s&vid=%s&vnonce=%s&vdigest=%s" % (full_url,self.apiid,nonce,digest)
         headers = {'User-Agent':'Apache-HttpClient/UNAVAILABLE (java 1.4)'}#,'Accept-Encoding':'gzip'}
-        req = urllib2.Request(requrl,headers=headers,data=data)
+        req = urllib2.Request(full_url,headers=headers,data=data)
         res = urllib2.urlopen(req).read()
         return res
 
-    def parse_regfile(self):
-        with open(self.regfile) as f:
-            self.apiid,self.token = f.read().split(':')
-
-    def register(self):
-        headers = {'User-Agent':'Apache-HttpClient/UNAVAILABLE (java 1.4)','Content-Type':'text/plain; charset=UTF-8'}
-        url = 'http://clientreg.vervewireless.com/locales'
-        data = '<localereq version="1.0"><mobileapp id="com.raycom.hnn" version="3.3.13.0" /></localereq>'
-        req = urllib2.Request(url,data=data,headers=headers)
-        res = urllib2.urlopen(req).read()
-        xml = ET.fromstring(res)
-        for locales in xml:
-            for locale in locales:
-                if locale.attrib['name'] == 'HNN for Android':
-                    locale_key = locale.attrib['locale_key']
-            break
-        else:
-            print "Couldn't get the locale_key"
-            return
-        deviceid = hashlib.md5(str(random.random())).hexdigest()
-        url="http://clientreg.vervewireless.com/register"
-        headers = {'User-Agent':'Apache-HttpClient/UNAVAILABLE (java 1.4)','Content-Type':'application/xml'}
-        data = '<registerreq version="1.0"><mobileapp id="com.raycom.hnn" name="" version="3.3.13.0" /><mobiledevice uniqueid="' + deviceid + \
-                '" vendor="LGE" model="LGLS990" firmware="5.0.1" /><mobilelocale locale="'+ locale_key +'" /></registerreq>'
-        req = urllib2.Request(url,data=data,headers=headers)
-        res = urllib2.urlopen(req).read()
-        self.apiid = res.split('apiauth id="')[1].split('"',1)[0]
-        self.token = res.split('token="')[1].split('"',1)[0]
-        # Write them out to a file
-        with open(self.regfile,'w') as f:
-            f.write("%s:%s" % (self.apiid,self.token))
-        return
-
     def get_root_menu(self):
+        #KHON2
         icon = "https://lintvkhon.files.wordpress.com/2014/04/logo-khon2-large.png"
         url = 'http://khon2.com/live-stream'
 	self.add_link(icon,{'Title':'Watch KHON2 Live'},{'name':'Watch KHON2 Live','action':'live','url':url},icon)
-        url = self.baseurl + '/hierarchy?pageName=default_client'
-        res = self.do_http(url)
-        xml = ET.fromstring(res)
-        for videotree in xml.findall(".//outline"):
-            if videotree.attrib['name'] == 'WATCH HNN NOW':
-                icon = videotree.attrib['iconUrl']
-                url = videotree.attrib['xmlUrl']
-                self.add_link(icon,
-                    {'Title':'Watch HNN Now','Plot':'Hawaii News Now live stream. Available: M-F 4:30-8am, 5-6:30pm, 9pm, 10pm; Sat & Sun 5pm, 9pm, 10pm'},
-                    {'name':'Watch HNN Now','action':'live','url':url},
-                    icon)
-            if videotree.attrib['name'] == 'WATCH K5 Live':
-                icon = videotree.attrib['iconUrl']
-                url = videotree.attrib['xmlUrl']
-                self.add_link(icon,{'Title':'Watch K5 Live'},{'name':'Watch K5 Live','action':'live','url':url},icon)
-            if videotree.attrib['name'] == 'Video':
-                icon = videotree.attrib['iconUrl']
-                for cat in videotree:
-                    name = cat.attrib['name']
-                    url = cat.attrib['xmlUrl']
-                    self.add_dir(self.icon,{'Title':name},{'name':name,'action':'category','url':url},self.icon)
+        
+        #HNN Live
+        url = 'http://www.hawaiinewsnow.com/category/198303/livestream'
+        icon = self.icon
+        self.add_link(icon,{'Title':'Watch HNN Live'},{'name':'Watch HNN Live','action':'live','url':url},icon)
+
+        #K5 Live
+        url = 'http://www.k5thehometeam.com/category/201839/livestream'
+        icon = "https://vignette.wikia.nocookie.net/logopedia/images/a/aa/KFVE_2009.png/revision/latest?cb=20110424091952"
+        self.add_link(icon,{'Title':'Watch K5 Live'},{'name':'Watch K5 Live','action':'live','url':url},icon)
+
+        for cid in CATEGORY_IDS:
+            url = self.baseurl + '/feed/categories/%s/clips?apikey=%s&context-affiliate-id=%s&alt=json' % (cid,self.apikey,self.affiliate_id)
+            # get the icon
+            res = json.loads(self.do_http(url+"&results=1"))
+            fanart = icon = res['rss']['channel']['image']['url']
+            title = res['rss']['channel']['title']
+            self.add_dir(icon,{'Title':title},{'name':title,'action':'category','url':url+'&results=200'},icon)
 
     def play_live_stream(self,url):
-        '''
-        playtimes=[(datetime.time(4,30,00),datetime.time(8,00,00)),
-                (datetime.time(17,00,00),datetime.time(18,30,00)),
-                (datetime.time(21,00,00),datetime.time(21,30,00)),
-                (datetime.time(22,00,00),datetime.time(22,30,00))]
-        now = datetime.datetime.now().time()
-        for span in playtimes:
-            if now > span[0] and now < span[1]:
-                break
-        else:
-            dialog = xbmcgui.Dialog()
-            dialog.ok("Live Stream", "Content is LIVE only during these times (HST):", "Weekdays - 4:30am to 8am, 5pm to 6:30pm, 9pm, 10pm",
-                    "Weekends - 5pm, 9pm, 10pm")
-        '''
-        res = urllib2.urlopen(url).read()
-        for url in re.findall("\"(https?://new.livestream.com.*)\"",res): 
-            res = urllib2.urlopen(url).read()
+        res = self.do_http(url)
+        for url in re.findall(r"\"(https?\://(?:new\.)?livestream\.com.*)\"",res): 
+            res = self.do_http(url)
             jsontext = res.split('window.config = ')[1].split(';</script>')[0]
             js = json.loads(jsontext)
             m3u8_url = js['event']['stream_info']['m3u8_url']
@@ -209,7 +141,10 @@ class Plugin():
             cookie = '; '.join(cookies)
             res = res.read()
             resolutions = re.findall("RESOLUTION=(\d{3,4}x\d{3,4})",res)
-            high_res = sorted(resolutions)[-1]
+            if len(resolutions) >= self.quality:
+                high_res = sorted(resolutions)[self.quality]
+            else:
+                high_res = sorted(resolutions)[-1]
             play_m3u8 = None
             for i,line in enumerate(res.splitlines()):
                 if high_res in line:
@@ -223,17 +158,35 @@ class Plugin():
                 xbmc.Player().play(item=play_m3u8 + appstr,listitem=xbmcgui.ListItem(path=url))
 
     def videos_from_category(self,url):
-        ns = {'media':"http://search.yahoo.com/mrss/"}
-        xml = ET.fromstring(self.do_http(url))
-        for itemtree in xml.findall(".//item"):
-            title = itemtree.find('title').text.encode('utf8')
-            for content in itemtree.findall('.//{%s}content' % ns['media']):
-                if content.attrib['type'] == 'video/mp4':
-                    url = content.attrib['url']
-                    if url[-4:] == '.f4m': continue
-                    icon = itemtree.find('.//{%s}thumbnail' % ns['media']).attrib['url']
-                    self.add_link(icon,{'TItle':title},{'name':title,'action':'play','url':url},icon)
+        js = json.loads(self.do_http(url))
+        for item in js['rss']['channel']['item']:
+            try:
+                title = item['title'].encode('utf8')
+                icon = item['media:thumbnail'][0]['@url']
+                videos = []
+                for media in item['media:group']['media:content']:
+                    if media["@type"] != "video/mp4": continue
+                    videos.append(media)
+            except:
+                xbmc.log("Couldn't find videos for '%s'" % title,xbmc.LOGERROR)
+                continue
+            if not videos:
+                xbmc.log("Couldn't find videos for '%s'" % title,xbmc.LOGERROR)
+                for video in videos:
+                    xbmc.log("%s" % video['@url'],xbmc.LOGERROR)
+                continue
+            the_video = None
+            for video in videos:
+                if video['@height'] == self.quality:
+                    the_video = video
                     break
+            else:
+                the_video = videos[0]
+
+            url = the_video['@url']
+            duration = the_video['@duration']
+            self.add_link(icon,{'Title':title},{'name':title,'action':'play','url':url},icon)
+            #xbmc.log("Adding video %s" % title,xbmc.LOGERROR)
 
 def main():
     xbmcplugin.setContent(HANDLE, 'tvshows')
@@ -253,10 +206,7 @@ def main():
         action=None
 
     plugin = Plugin()
-    if os.path.exists(plugin.regfile):
-        plugin.parse_regfile()
     if action==None:
-        plugin.register()
         plugin.get_root_menu()
 
     elif action=='category':
